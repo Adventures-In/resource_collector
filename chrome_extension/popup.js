a8362fea7393dbe0e2f45cf86ea26e801c2921e8
Nick Meinhold <nick.meinhold@gmail.com>
2021-06-06 00:33:03 +1000

    Get chrome.identity working

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -7,12 +7,18 @@ chrome.storage.sync.get("color", ({ color }) => {
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+  chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+    // Use the token.
+    chrome.tabs.create({url: 'index.html'});
   });
+
+  /////////// change background color 
+  // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  // chrome.scripting.executeScript({
+  //   target: { tabId: tab.id },
+  //   function: setPageBackgroundColor,
+  // });
 });
 
 // The body of this function will be executed as a content script inside the
